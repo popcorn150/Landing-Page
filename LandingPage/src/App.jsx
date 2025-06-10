1dbb64f9bdafab35a3ac947dad0e394322effd97
popcorn150 <hungrymanchannel@gmail.com>
2025-06-10 22:39:12 +0100

    reactor images

--- LandingPage/src/App.jsx
+++ LandingPage/src/App.jsx
@@ -1,13 +1,14 @@
 import { exchanges, steps } from "./data/data";
+import { Background, Doll, FooterLabubuIcon, HowToLabubu, JoinUsIcon, LabubuIcon, LabubuTokenIcon, MainTitle, Title, WhoWeAreIcon, WhoWeLabubuIcon, WLabubuIcon } from "./utils";
 
 export default function App() {
   return (
-    <div style={{ backgroundImage: 'url(./public/background.png)' }} className="bg-cover bg-center min-h-screen">
+    <div style={{ backgroundImage: 'url(../src/imgs/background.png)' }} className="bg-cover bg-center min-h-screen">
       {/* Main Container */}
       <div className="text-black mx-15 font-sans">
         {/* Navbar */}
         <header className="flex justify-between items-center px-6 py-4">
-          <img src="/public/title.png" alt="LABUBU" className="hover:cursor-pointer" />
+          <img src={Title} alt="LABUBU" className="hover:cursor-pointer" />
           <nav className="space-x-20 text-sm md:text-base">
             <a href="#" className="hover:text-pink-300 hover:cursor-pointer text-[#742561]">What is a Labubu?</a>
             <a href="#" className="hover:text-pink-300 hover:cursor-pointer text-[#742561]">About Us</a>
@@ -18,7 +19,7 @@ export default function App() {
 
         {/* Hero Section */}
         <section className="text-center py-12 px-4">
-          <img src="/public/mainTitle.png" alt="LABUBU" className="mx-auto w-xl md:w-lg drop-shadow-md" />
+          <img src={MainTitle} alt="LABUBU" className="mx-auto w-xl md:w-lg drop-shadow-md" />
           <p className="text-xl font-light text-[#83296D] tracking-widest mt-2 mb-6">
             the original fan token on solana
           </p>
@@ -31,7 +32,7 @@ export default function App() {
           </p>
           <div className="">
             {/* Hero Image Placeholder */}
-            <img src="/public/doll-1.png" alt="Doll-1" className="w-[400px] h-[400px] mx-auto" />
+            <img src={Doll} alt="Doll-1" className="w-[400px] h-[400px] mx-auto" />
           </div>
         </section>
 
@@ -48,10 +49,10 @@ export default function App() {
         {/* What is a Labubu */}
         <section className="mt-16 px-6 flex flex-wrap justify-between items-center">
           <div className="mb-4">
-            <img src="/public/labubuImg.png" className="w-xs" />
+            <img src={LabubuIcon} className="w-xs" />
           </div>
           <div className="flex flex-end">
-            <img src="/public/w-labubu.png" alt="What is Labubu?" className="w-sm mb-4" />
+            <img src={WLabubuIcon} alt="What is Labubu?" className="w-sm mb-4" />
             <p className="max-w-md mx-auto text-start text-sm">
               Labubu is a character in a series of stories called "The Monsters" by Hong-Kong born artist Kasing Lung. Inspired by Nordic fairy tales, he created this universe in 2015...
             </p>
@@ -60,18 +61,18 @@ export default function App() {
 
         {/* Who We Are */}
         <section className="mt-16 px-6 flex flex-wrap justify-between items-center">
-          <img src="/public/whoweare.png" alt="Who We Are" className="w-sm mb-4" />
+          <img src={WhoWeAreIcon} alt="Who We Are" className="w-sm mb-4" />
           <p className="max-w-md mx-auto text-start text-sm">
             Labubu is a character in a series of stories called "The Monsters" by Hong-Kong born artist Kasing Lung...
           </p>
           <div className="mb-4">
-            <img src="/public/whowelabubu.png" alt="Who we are" className="w-sm mb-4" />
+            <img src={WhoWeLabubuIcon} alt="Who we are" className="w-sm mb-4" />
           </div>
         </section>
 
         {/* How to $LABUBU */}
         <section className="mt-20 px-6 text-center">
-          <img src="/public/howtolabubu.png" alt="How To $Labubu" className="w-md mx-auto" />
+          <img src={HowToLabubu} alt="How To $Labubu" className="w-md mx-auto" />
           <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
             {steps.map(({ id, title, description, icon }) => (
               <div key={id} alt={title} className="bg-white/20 rounded-2xl p-4 text-center">
@@ -85,7 +86,7 @@ export default function App() {
 
         {/* Tokenomics */}
         <section className="mt-20 px-6 text-center">
-          <img src="/public/token.png" alt="How To $Labubu" className="w-md mx-auto mb-5" />
+          <img src={LabubuTokenIcon} alt="Toeknomics" className="w-md mx-auto mb-5" />
           <div className="flex flex-col md:flex-row justify-center gap-6 text-center mb-4">
             <div className="bg-white border-3 border-[#8C4DA3] px-10 py-4 rounded-xl drop-shadow-md">0%<br />Fees</div>
             <div className="bg-white border-3 border-[#8C4DA3] px-10 py-4 rounded-xl drop-shadow-md">100%<br />Community</div>
@@ -96,16 +97,16 @@ export default function App() {
 
         {/* Join Us */}
         <section className="mt-20 px-6 text-center">
-          <img src="/public/joinus.png" alt="Join Us" className="w-2xs mx-auto mb-10" />
+          <img src={JoinUsIcon} alt="Join Us" className="w-2xs mx-auto mb-10" />
           <div className="flex justify-center space-x-4">
-            <button className="bg-[#1DA1F2] text-white font-normal px-10 py-3 rounded-full">Twitter</button>
-            <button className="bg-[#4267B2] text-white font-normal px-10 py-3 rounded-full">Facebook</button>
+            <button className="bg-[#1DA1F2] hover:cursor-pointer text-white font-normal px-10 py-3 rounded-full">Twitter</button>
+            <button className="bg-[#4267B2] hover:cursor-pointer text-white font-normal px-10 py-3 rounded-full">Facebook</button>
           </div>
         </section>
 
         {/* Footer */}
         <footer className="mt-30 text-center text-xs py-6">
-          <img src="/public/footerlabubu.png" alt="Labubu" className="w-xs mx-auto" />
+          <img src={FooterLabubuIcon} alt="Labubu" className="w-xs mx-auto" />
           <nav className="space-x-10 my-5 text-sm md:text-base">
             <a href="#" className="hover:cursor-pointer text-[#1E0E62]">Portfolio?</a>
             <a href="#" className="hover:cursor-pointer text-[#1E0E62]">How it Works</a>
--- LandingPage/src/data/data.js
+++ LandingPage/src/data/data.js
@@ -1,14 +1,16 @@
+import { Biconomy, BingX, Bitget, BitMart, CoffeeLabubu, CoinW, Gateio, LBank, MEXC, Phantom, Radiyum, Raydium, Solana, Weex } from "../utils";
+
 export const exchanges = [
-    { id: 1, name: 'Bitget', img: '/public/bitget.png' },
-    { id: 2, name: 'BingX', img: '/public/bingx.png' },
-    { id: 3, name: 'Gate.io', img: '/public/gateio.png' },
-    { id: 4, name: 'BitMart', img: '/public/bitmart.png' },
-    { id: 5, name: 'LBANK', img: '/public/lbank.png' },
-    { id: 6, name: 'Weex', img: '/public/weex.png' },
-    { id: 7, name: 'Biconomy', img: '/public/biconomy.png' },
-    { id: 8, name: 'Raydium', img: '/public/raydium.png' },
-    { id: 9, name: 'MEXC Global', img: '/public/mexc.png' },
-    { id: 10, name: 'CoinW', img: '/public/coinw.png' }
+    { id: 1, name: 'Bitget', img: Bitget },
+    { id: 2, name: 'BingX', img: BingX },
+    { id: 3, name: 'Gate.io', img: Gateio },
+    { id: 4, name: 'BitMart', img: BitMart },
+    { id: 5, name: 'LBANK', img: LBank },
+    { id: 6, name: 'Weex', img: Weex },
+    { id: 7, name: 'Biconomy', img: Biconomy },
+    { id: 8, name: 'Raydium', img: Raydium },
+    { id: 9, name: 'MEXC Global', img: MEXC },
+    { id: 10, name: 'CoinW', img: CoinW }
 ];
 
 export const steps = [
@@ -16,24 +18,24 @@ export const steps = [
         id: 1,
         title: 'STEP 1',
         description: 'Download Phantom',
-        icon: '/public/phantom.png'
+        icon: Phantom
     },
     {
         id: 2,
         title: 'STEP 2',
         description: 'Buy Or Transfer $SOL',
-        icon: '/public/solana.png'
+        icon: Solana
     },
     {
         id: 3,
         title: 'STEP 3',
         description: 'Go To Raydium.io / Swap And Search $LABUBU',
-        icon: '/public/raydium.png'
+        icon: Radiyum
     },
     {
         id: 4,
         title: 'STEP 4',
         description: 'Congrats New $LABUBU HOLDER!',
-        icon: '/public/labubu.png'
+        icon: CoffeeLabubu
     }
 ];
