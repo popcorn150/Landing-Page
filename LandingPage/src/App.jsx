673ef3135eb5487cec1689097ca2f02d0639d071
popcorn150 <hungrymanchannel@gmail.com>
2025-06-11 01:44:34 +0100

    added fonts and fixed the remaining few styling issues..

--- LandingPage/src/App.jsx
+++ LandingPage/src/App.jsx
@@ -1,106 +1,146 @@
-import { exchanges, steps } from "./data/data";
-import { Background, Doll, FooterLabubuIcon, HowToLabubu, JoinUsIcon, LabubuIcon, LabubuTokenIcon, MainTitle, Title, WhoWeAreIcon, WhoWeLabubuIcon, WLabubuIcon } from "./utils";
+import './styles/font.css'
+import {
+  exchanges,
+  steps,
+} from "./data/data";
+import {
+  Doll,
+  FooterLabubuIcon,
+  HowToLabubu,
+  JoinUsIcon,
+  LabubuIcon,
+  LabubuTokenIcon,
+  MainTitle,
+  Title,
+  WhoWeAreIcon,
+  WhoWeLabubuIcon,
+  WLabubuIcon,
+} from "./utils";
 
 export default function App() {
   return (
-    <div className="bg-[url(../src/imgs/background.png)] bg-cover bg-center min-h-screen font-sans text-black">
+    <div className="bg-[url(../src/imgs/background.png)] bg-cover bg-center min-h-screen font-sans text-black overflow-x-hidden">
       {/* Navbar */}
-      <header className="flex flex-col md:flex-row justify-between items-center px-6 py-4 gap-4">
+      <header className="max-w-screen-xl mx-auto px-6 py-4 flex flex-col md:flex-row justify-between items-center gap-4">
         <img src={Title} alt="LABUBU" className="hover:cursor-pointer" />
         <nav className="flex flex-wrap justify-center gap-6 text-sm md:text-base text-[#742561]">
-          <a href="#" className="hover:text-pink-300 hover:cursor-pointer">What is a Labubu?</a>
-          <a href="#" className="hover:text-pink-300 hover:cursor-pointer">About Us</a>
-          <a href="#" className="hover:text-pink-300 hover:cursor-pointer">How to $LABUBU</a>
-          <a href="#" className="hover:text-pink-300 hover:cursor-pointer">Tokenomics</a>
+          <a href="#" className="hover:text-pink-300">What is a Labubu?</a>
+          <a href="#" className="hover:text-pink-300">About Us</a>
+          <a href="#" className="hover:text-pink-300">How to $LABUBU</a>
+          <a href="#" className="hover:text-pink-300">Tokenomics</a>
         </nav>
       </header>
 
       {/* Hero Section */}
-      <section className="text-center py-12 px-4">
+      <section className="relative text-center py-20 px-4 max-w-screen-xl mx-auto">
         <img src={MainTitle} alt="LABUBU" className="mx-auto w-60 md:w-96 drop-shadow-md" />
-        <p className="text-lg text-[#83296D] tracking-widest mt-2 mb-6">
+        <p className="font-sans text-lg text-[#83296D] tracking-widest mt-2 mb-6">
           the original fan token on solana
         </p>
-        <div className="flex flex-col sm:flex-row justify-center items-center gap-4 mb-4">
-          <button className="bg-[#FB89E1] hover:bg-pink-600 hover:text-white hover:cursor-pointer text-[#4F1C42] font-semibold text-base py-2 px-20 rounded-full">View Chart</button>
-          <button className="bg-blue-500 hover:cursor-pointer text-white font-semibold py-2 px-15 rounded-full">Join Community</button>
+        <div className="font-sans flex flex-col sm:flex-row justify-center items-center gap-4 mb-4">
+          <button className="bg-[#FB89E1] text-[#4F1C42] font-semibold py-2 px-10 rounded-full hover:bg-pink-600 hover:text-white">
+            View Chart
+          </button>
+          <button className="bg-blue-500 text-white font-semibold py-2 px-10 rounded-full">
+            Join Community
+          </button>
         </div>
-        <p className="text-sm text-white bg-black rounded-full inline-block px-20 py-2 mt-2">
+        <p className="font-sans text-sm text-white bg-black rounded-full inline-block px-6 py-2 mt-2">
           JB2wozZLdzVfnaCFHxLg93R5Rh5iTH7ixEDWJQ0pump
         </p>
-        <div className="">
-          {/* Hero Image Placeholder */}
-          <img src={Doll} alt="Doll-1" className="w-64 md:w-[400px] mx-auto mt-6" />
-        </div>
+        <img src={Doll} alt="Doll" className="w-64 md:w-[400px] mx-auto mt-6 z-10 relative" />
       </section>
 
       {/* Exchanges */}
-      <section className="text-center px-4 mt-12">
-        <h2 className="font-bold text-xl mb-4">EXCHANGES WE ARE ON</h2>
+      <section className="text-center py-20 px-6 max-w-screen-xl mx-auto">
+        <h2 className="exchanges font-bold text-3xl mb-6">EXCHANGES WE ARE ON</h2>
         <div className="flex flex-wrap justify-center gap-6 md:gap-10">
           {exchanges.map(({ id, name, img }) => (
-            <img key={id} src={img} alt={name} className="w-40 h-20 object-contain" />
+            <img
+              key={id}
+              src={img}
+              alt={name}
+              className="w-36 h-16 object-contain"
+            />
           ))}
         </div>
       </section>
 
-      {/* What is a Labubu */}
-      <section className="mt-16 px-6 flex flex-col md:flex-row items-center gap-8">
-        <img src={LabubuIcon} className="w-40" />
-        <div className="flex flex-col items-start">
+      {/* What is Labubu */}
+      <section className="relative py-20 px-6 max-w-screen-xl mx-auto flex flex-col md:flex-row items-center gap-10">
+        <img src={LabubuIcon} alt="Labubu" className="w-40 md:w-60 z-10" />
+        <div className="text-left z-10">
           <img src={WLabubuIcon} alt="What is Labubu?" className="w-60 mb-4" />
-          <p className="max-w-md text-sm text-left">
-            Labubu is a character in a series of stories called "The Monsters" by Hong-Kong born artist Kasing Lung. Inspired by Nordic fairy tales, he created this universe in 2015...
+          <p className="font-sans text-sm text-[#1E1E1E] max-w-md">
+            Labubu is a character in a series of stories called "The Monsters" by Hong-Kong born artist Kasing Lung.
+            Inspired by Nordic fairy tales, he created this universe in 2015, with characters modeled after elves, fairies, and monsters.
+          </p>
+          <br />
+          <p className="font-sans text-sm text-[#1E1E1E] max-w-md">
+            From bustling Asian cities to the streets of Europe, Labubu has transcended borders,
+            uniting fans of all ages in a shared love for this adorable toy.
           </p>
         </div>
       </section>
 
       {/* Who We Are */}
-      <section className="mt-16 px-6 flex flex-col md:flex-row items-center gap-8">
-        <img src={WhoWeAreIcon} alt="Who We Are" className="w-60" />
-        <p className="max-w-md text-sm text-left">
-          Labubu is a character in a series of stories called "The Monsters" by Hong-Kong born artist Kasing Lung...
+      <section className="relative py-20 px-6 max-w-screen-xl mx-auto flex flex-col md:flex-row items-center gap-10">
+        <img src={WhoWeAreIcon} alt="Who We Are" className="w-60 order-1 md:order-none" />
+        <p className="font-sans text-sm text-[#1E1E1E] max-w-md order-3 md:order-none">
+          Labubu is a character in a series of stories called "The Monsters" by Hong-Kong born artist Kasing Lung.
+          Inspired by Nordic fairy tales, he created this universe in 2015, with characters modeled after elves, fairies, and monsters.
         </p>
-        <img src={WhoWeLabubuIcon} alt="Who we are" className="w-60" />
+        <br />
+        <p className="font-sans text-sm text-[#1E1E1E] max-w-md order-3 md:order-none">
+          From bustling Asian cities to the streets of Europe, Labubu has transcended borders, 
+          uniting fans of all ages in a shared love for this adorable toy.
+        </p>
+        <img src={WhoWeLabubuIcon} alt="Who we are" className="w-60 order-2 md:order-none" />
       </section>
 
       {/* How to $LABUBU */}
-      <section className="mt-20 px-6 text-center">
-        <img src={HowToLabubu} alt="How To $Labubu" className="w-64 md:w-80 mx-auto mb-6" />
+      <section className="py-15 px-6 text-center max-w-screen-xl mx-auto">
+        <img src={HowToLabubu} alt="How To $Labubu" className="w-64 md:w-80 mx-auto mb-10" />
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6">
           {steps.map(({ id, title, description, icon }) => (
-            <div key={id} alt={title} className="bg-white/20 rounded-2xl p-4">
-              <img src={icon} alt={title} className="w-16 h-16 mx-auto mb-2" />
-              <p className="text-sm font-semibold">{title}</p>
-              <p className="text-xs">{description}</p>
+            <div key={id} className="bg-[#F2B0FF] rounded-2xl p-4">
+              <img src={icon} alt={title} className="w-20 h-20 mx-auto mb-2" />
+              <p className="steps text-base font-semibold">{title}</p>
+              <p className="text-xs font-semibold font-sans ">{description}</p>
             </div>
           ))}
         </div>
       </section>
 
       {/* Tokenomics */}
-      <section className="mt-20 px-6 text-center">
-        <img src={LabubuTokenIcon} alt="Toeknomics" className="w-64 md:w-80 mx-auto mb-6" />
-        <div className="flex flex-col md:flex-row justify-center gap-6 mb-4">
-          <div className="bg-white border-3 border-[#8C4DA3] px-10 py-4 rounded-xl shadow">0%<br />Fees</div>
-          <div className="bg-white border-3 border-[#8C4DA3] px-10 py-4 rounded-xl shadow">100%<br />Community</div>
-          <div className="bg-white border-3 border-[#8C4DA3] px-10 py-4 rounded-xl shadow">LP<br />Locked</div>
+      <section className="py-20 px-6 text-center max-w-screen-xl mx-auto">
+        <img src={LabubuTokenIcon} alt="Tokenomics" className="w-64 md:w-80 mx-auto mb-10" />
+        <div className="flex flex-col md:flex-row justify-center gap-6 mb-6">
+          <div className="tokenomics bg-white border-3 border-[#8C4DA3] px-7 py-4 rounded-xl shadow-2xl">
+            <span className='text-5xl'>0%</span> <br /><span className='text-base'>Fees</span>
+          </div>
+          <div className="tokenomics bg-white border-3 border-[#8C4DA3] px-7 py-4 rounded-xl shadow-2xl">
+            <span className='text-5xl'>100%</span> <br /><span className='text-base'>Community</span>
+          </div>
+          <div className="tokenomics bg-white border-3 border-[#8C4DA3] px-7 py-4 rounded-xl shadow-2xl">
+            <span className='text-5xl'>LP</span> <br /><span className='text-base'>Locked</span>
+          </div>
         </div>
-        <p className="text-xl font-bold">SUPPLY:<br />1,000,000,000</p>
+        <p className="supply-rate text-3xl font-bold">SUPPLY:<br />1,000,000,000</p>
       </section>
 
       {/* Join Us */}
-      <section className="mt-20 px-6 text-center">
+      <section className="py-7 px-6 text-center max-w-screen-xl mx-auto">
         <img src={JoinUsIcon} alt="Join Us" className="w-40 mx-auto mb-10" />
         <div className="flex flex-col sm:flex-row justify-center items-center gap-4">
-          <button className="bg-[#1DA1F2] hover:cursor-pointer text-white font-normal px-10 py-3 rounded-full">Twitter</button>
-          <button className="bg-[#4267B2] hover:cursor-pointer text-white font-normal px-10 py-3 rounded-full">Facebook</button>
+          <button className="bg-[#1DA1F2] text-white px-10 py-3 rounded-full hover:cursor-pointer">Twitter</button>
+          <button className="bg-[#4267B2] text-white px-10 py-3 rounded-full hover:cursor-pointer">Facebook</button>
         </div>
       </section>
 
       {/* Footer */}
-      <footer className="mt-20 px-6 text-center text-xs py-10">
-        <img src={FooterLabubuIcon} alt="Labubu" className="w-40 mx-auto mb-4" />
+      <footer className="py-20 px-6 text-center text-xs max-w-screen-xl mx-auto">
+        <img src={FooterLabubuIcon} alt="Labubu" className="w-40 mx-auto mb-6" />
         <nav className="flex flex-wrap justify-center gap-6 text-sm md:text-base text-[#1E0E62] mb-4">
           <a href="#" className="hover:cursor-pointer text-[#1E0E62]">Portfolio?</a>
           <a href="#" className="hover:cursor-pointer text-[#1E0E62]">How it Works</a>
@@ -109,11 +149,7 @@ export default function App() {
           <a href="#" className="hover:cursor-pointer text-[#1E0E62]">Contacts</a>
         </nav>
         <p className="text-gray-600 text-sm max-w-2xl mx-auto">
-          This meme coin is not affiliated with, endorsed by, or associated with the official Labubu brand,
-          Popmart company, or any of its representatives. This project is purely for entertainment purposes
-          and does not claim any rights or connections to Popmart or its intellectual property. Please do
-          your own research before participating, and remember that meme coins are highly speculative and
-          volatile assets.
+          This meme coin is not affiliated with, endorsed by, or associated with the official Labubu brand, Popmart company, or any of its representatives. This project is purely for entertainment purposes and does not claim any rights or connections to Popmart or its intellectual property. Please do your own research before participating, and remember that meme coins are highly speculative and volatile assets.
         </p>
       </footer>
     </div>
